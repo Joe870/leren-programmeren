a72952a17ff3fa3e4f6ddd0559e8be28942cc8b5
john doe <johndoe@example.com>
2022-09-21 14:26:36 +0200

    initial

---
+++ Module 1 - vliegende start/hello.py
@@ -0,0 +1,12 @@
+
+print('Hello wereld van…')
+print(''''   pppp    y     y   ttttttt   h   h    oooo   nnnn       
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n
+
+ ''')
+ # Joëlle van Breugel 99075308
---
+++ Module 2 - omgaan met/Feestlunch.py
@@ -0,0 +1,6 @@
+omschrijving "A" = "croissantjes"
+aantal = 17
+stuksprijs = 0.39
+print(aantal * stuksprijs)
+print(f"{aantal}{omschrijving}")
+print(f"{aantal} {omschrijving} {stuksprijs}") 

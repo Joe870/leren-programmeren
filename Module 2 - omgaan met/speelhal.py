4e9d78b015b2063d366f95bbeb87191de8470239
john doe <johndoe@example.com>
2022-09-22 11:42:14 +0200

    eerste commit

---
+++ Module 2 - omgaan met/speelhal.py
@@ -0,0 +1,12 @@
+aantal_personen = 4 
+toegangsprijs = 7.45
+prijs_gameseat = 0.37 
+tijd_gameseat = 5 
+totale_tijd_gameseat = 45 
+totale_tijd_gameseat = prijs_gameseat * 9 
+totale_toegangsprijs = aantal_personen * toegangsprijs 
+totale_prijs_gameseat = totale_tijd_gameseat * aantal_personen 
+totale_prijs = totale_toegangsprijs + totale_prijs_gameseat 
+print(totale_toegangsprijs)
+print(totale_prijs_gameseat)
+print(totale_prijs)

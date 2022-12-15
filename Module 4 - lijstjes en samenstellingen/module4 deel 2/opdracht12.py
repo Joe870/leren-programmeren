d4e3d382dd53cb5b2af27c720cea2b12557208a6
john doe <johndoe@example.com>
2022-12-15 10:02:48 +0100

    'oefening'

--- Module 4 - lijstjes en samenstellingen/module4 deel 2/opdracht12.py
+++ Module 4 - lijstjes en samenstellingen/module4 deel 2/opdracht12.py
@@ -1,13 +1,11 @@
 from fruitmand import fruitmand
+fruitmand.sort(reverse = True, key=lambda fruit:len(fruit['name']))
 nieuwe_fruitmand = {}
-def myFunc(e):
-    return len(e)
-fruitmand.sort(reverse = True, key = lambda fruit:fruit['name'],key=myFunc) 
 print(fruitmand)
 nieuwe_fruitmand.update(fruitmand[0])
 print(nieuwe_fruitmand)
 kleur = nieuwe_fruitmand['color'] = 'oranje'
-gewicht = nieuwe_fruitmand['weight'] / 1000
+gewicht = fruitmand[0]['weight'] / 1000
 vrucht = nieuwe_fruitmand['name']
 lengte_vrucht = len(nieuwe_fruitmand['name'])
-print(f'de {vrucht}{lengte_vrucht} heeft een{kleur} en heeft een gewicht van {gewicht}')
+print(f'de {vrucht} {lengte_vrucht} letters heeft een {kleur} kleur en heeft een gewicht van {gewicht} kg')

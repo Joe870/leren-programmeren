227253714551b2c5ac042b972f617f83f315b368
john doe <johndoe@example.com>
2023-06-14 13:22:04 +0200

    'ijssalon'

--- Module 5 - meer functions/ijssalon/functions-ijssalon.py
+++ Module 5 - meer functions/ijssalon/functions-ijssalon.py
@@ -2,27 +2,48 @@ from library_ijssalon import *
 prijs_bolletjes = 1.10
 prijs_hoorntjes = 1.25
 prijs_bakjes = 0.75
-aantal_bolletjes = 0
+aantal_aardbei = 0
+aantal_chocolade = 0
+aantal_munt = 0 
+aantal_vanille = 0
+totale_aantal_bolletjes = 0
 aantal_hoorntjes = 0
 aantal_bakjes = 0 
 volgende_bestelling = 'ja'
 hoorn_of_bak = ''
 
 while volgende_bestelling == "ja":
+    print('welkom bij Papi Gelato')
     aantal_bolletjes = aantal_bolletjes_ijs()
+    totale_aantal_bolletjes += aantal_bolletjes
     hoorn_of_bak = waar_in(aantal_bolletjes)
     meld_transactie = print(f'hier is uw {hoorn_of_bak} met {aantal_bolletjes} bolletje(s)')
+    for bolletjes in range(aantal_bolletjes):
+        smaken = smaak(aantal_bolletjes,bolletjes)
+        aantal_aardbei, aantal_chocolade, aantal_munt, aantal_vanille = aantal_smaak(aantal_aardbei,aantal_chocolade,aantal_munt,aantal_vanille,smaken)
     volgende_bestelling = meer()
-
-aantal_hoorntjes = aantal_hoorntjes_bakjes(aantal_bolletjes)
-eind_prijs_bolletjes = round(prijs_bolletjes * aantal_bolletjes,2)
-eind_prijs_hoorntjes = round(prijs_hoorntjes * aantal_hoorntjes,2)
-eind_prijs_bakjes = round(prijs_bakjes * aantal_bakjes,2) 
-eind_prijs_totaal = prijs_bakjes + prijs_hoorntjes + prijs_bolletjes
+    aantal_hoorntjes, aantal_bakjes = aantal_hoorntjes_bakjes(hoorn_of_bak,aantal_hoorntjes,aantal_bakjes)
+    print(f'{aantal_aardbei} aardbei')
+    print(f'{aantal_chocolade} schocola')
+    print(f'{aantal_munt} munt')
+    print(f'{aantal_vanille} vanille')
+    eind_prijs_aardbei = round(aantal_aardbei * prijs_bolletjes,2)
+    eind_prijs_chocola = round(aantal_chocolade * prijs_bolletjes,2)
+    eind_prijs_munt = round(aantal_munt * prijs_bolletjes,2)
+    eind_prijs_vanille = round(aantal_vanille * prijs_bolletjes,2)
+    eind_prijs_hoorntjes = round(aantal_hoorntjes * prijs_hoorntjes,2)
+    eind_prijs_bakjes = round(aantal_bakjes * prijs_bakjes,2)
+    eind_prijs_totaal = eind_prijs_bakjes + eind_prijs_hoorntjes + eind_prijs_aardbei + eind_prijs_chocola + eind_prijs_munt + eind_prijs_vanille
 
 print('-----------["papi gelato"]------------')
-if aantal_bolletjes >= 1:
-    print(f'Bolletjes      {aantal_bolletjes} x ${prijs_bolletjes} = $ {eind_prijs_bolletjes}')
+if aantal_aardbei >= 1:
+    print(f'Bolletjes aardbei      {aantal_aardbei} x ${prijs_bolletjes} = $ {eind_prijs_aardbei}')
+if aantal_chocolade >= 1:
+    print(f'Bolletjes chocola     {aantal_chocolade} x ${prijs_bolletjes} = $ {eind_prijs_chocola}')
+if aantal_munt >= 1:
+    print(f'Bolletjes munt     {aantal_munt} x ${prijs_bolletjes} = $ {eind_prijs_munt}')
+if aantal_vanille >= 1:
+    print(f'Bolletjes vanille      {aantal_vanille} x ${prijs_bolletjes} = $ {eind_prijs_vanille}')
 if aantal_hoorntjes >= 1:
     print(f'Hoorntjes      {aantal_hoorntjes} x ${prijs_hoorntjes} = $ {eind_prijs_hoorntjes}')
 if aantal_bakjes >= 1:
--- Module 5 - meer functions/ijssalon/library_ijssalon.py
+++ Module 5 - meer functions/ijssalon/library_ijssalon.py
@@ -1,4 +1,18 @@
+prijs_bolletjes = 1.10
+prijs_hoorntjes = 1.25
+prijs_bakjes = 0.75
+aantal_bolletjes = 0
+aantal_hoorntjes = 0
+aantal_bakjes = 0 
 volgende_bestelling = 'ja'
+hoorn_of_bak = ''
+aantal = 0
+aantal_aardbei = 0
+aantal_chocolade = 0
+aantal_munt = 0 
+aantal_vanille = 0
+volgende_bestelling = 'ja'
+
 def aantal_bolletjes_ijs():
     aantal_bolletjes = int(input('hoeveel bolletjes'))
     print(aantal_bolletjes)
@@ -11,25 +25,16 @@ def waar_in(aantal_bolletjes):
     hofb = ''
     if aantal_bolletjes >3 and aantal_bolletjes <8:
         hofb = 'bakje'
-    else:
+    elif aantal_bolletjes>=1:
         hofb = input(f'wilt u deze {aantal_bolletjes} bolletje(s) in een hoorntje of een bakje?')
     return hofb
 
-def aantal_hoorntjes_bakjes(aantal_bolletjes):
-    aantal_bakjes = 0 
-    aantal_hoorntjes = 0 
-    prijs_hoorntjes = 1.25
-    prijs_bakjes = 0.75
-    if aantal_bolletjes >3 and aantal_bolletjes <=8:
+def aantal_hoorntjes_bakjes(hoorn_of_bak,aantal_hoorntjes,aantal_bakjes):
+    if hoorn_of_bak == 'bakje':
         aantal_bakjes += 1
-    hofb = waar_in(aantal_bolletjes)
-    if hofb == 'hoorntje':
+    elif hoorn_of_bak == 'hoorntje':
         aantal_hoorntjes += 1 
-    elif hofb == 'bakje':
-        aantal_bakjes += 1 
-    prijs_hoorntjes = round(prijs_hoorntjes * aantal_hoorntjes,2)
-    prijs_bakjes = round(prijs_bakjes * aantal_bakjes,2)
-    return prijs_hoorntjes, prijs_bakjes
+    return aantal_hoorntjes, aantal_bakjes
 
 def meer():
     doorgaan = input("wilt u nog meer bestellen")
@@ -37,3 +42,19 @@ def meer():
         print('bedankt en tot ziens')
     return doorgaan
 
+def smaak(aantal_bolletjes, bolletje):
+    smaak = input(f'welke smaak wilt u voor bolletje {bolletje}? A) aardbei, C) chocolade, M) munt of V) vanille?')
+    smaak=smaak.upper()
+    print(smaak) 
+    return smaak
+
+def aantal_smaak(aantal_aardbei, aantal_chocolade, aantal_munt, aantal_vanille,taste):
+    if taste == 'A':
+        aantal_aardbei +=1
+    elif taste == 'C':
+        aantal_chocolade +=1
+    elif taste == 'M':
+        aantal_munt +=1
+    elif taste == 'V':
+        aantal_vanille +=1
+    return aantal_aardbei, aantal_chocolade, aantal_munt, aantal_vanille

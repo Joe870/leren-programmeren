468990eed8985b449353a1cfdb59820fa94307fb
john doe <johndoe@example.com>
2023-06-21 20:36:51 +0200

    'ijssalon'

---
+++ Module 5 - meer functions/ijssalon/hoofdprogramma_ijssalon.py
@@ -0,0 +1,104 @@
+from library_ijssalon import *
+prijs_bolletjes = 0.95
+prijs_hoorntjes = 1.25
+prijs_bakjes = 0.75
+prijs_liter = 9.80
+aantal_aardbei = 0
+aantal_chocolade = 0
+aantal_vanille = 0
+aantal_bolletjes = 0
+aantal_liter = 0
+aantal_hoorntjes = 0
+aantal_bakjes = 0 
+aantal_topping = 0
+volgende_bestelling = 'ja'
+hoorn_of_bak = ''
+aantal_geen = 0 
+aantal_slagroom = 0
+aantal_sprinkels = 0 
+aantal_caramel_hoorn = 0
+aantal_caramel_bak = 0
+prijs_geen = 0
+prijs_slagroom = 0.50
+prijs_sprinkels = 0.30 
+prijs_caramel_hoorn = 0.60
+prijs_caramel_bakje = 0.90
+eind_prijs_slagroom = 0
+eind_prijs_sprinkels = 0
+eind_prijs_geen = 0
+eind_prijs_caramel_bakje = 0
+eind_prijs_caramel_hoorntje =0 
+btw = 6
+
+while volgende_bestelling == "ja":
+    print('welkom bij Papi Gelato')
+    klant = int(soort_klant())
+    if klant == 1:
+        aantal_bolletjes = aantal_bolletjes_ijs(klant)
+        aantal_geen, aantal_slagroom, aantal_sprinkels, aantal_caramel_bak, aantal_caramel_hoorn = welke_topping(aantal_bolletjes,hoorn_of_bak)
+        for bolletjes in range(aantal_bolletjes):
+            smaken = smaak(aantal_bolletjes, aantal_liter, klant)
+            aantal_aardbei, aantal_chocolade, aantal_vanille = aantal_smaak(aantal_aardbei,aantal_chocolade,aantal_vanille,smaken)
+        hoorn_of_bak = waar_in(aantal_bolletjes)
+        aantal_hoorntjes, aantal_bakjes = aantal_hoorntjes_bakjes(hoorn_of_bak,aantal_hoorntjes,aantal_bakjes)
+        meld_transactie = print(f'hier is uw {hoorn_of_bak} met {aantal_bolletjes} bolletje(s)')
+        volgende_bestelling = meer()
+        aantal_topping = aantal_geen + aantal_slagroom + aantal_sprinkels + aantal_caramel_bak + aantal_caramel_hoorn
+        eind_prijs_slagroom += round(aantal_slagroom * prijs_slagroom,2)
+        eind_prijs_sprinkels += round(aantal_sprinkels * prijs_sprinkels,2)
+        eind_prijs_geen += round(aantal_geen * prijs_geen,2)
+        eind_prijs_caramel_bakje += round(aantal_caramel_bak * prijs_caramel_bakje,2)
+        eind_prijs_caramel_hoorntje += round(aantal_caramel_hoorn * prijs_caramel_hoorn,2)
+        eind_prijs_topping = eind_prijs_slagroom + eind_prijs_sprinkels + eind_prijs_geen + eind_prijs_caramel_bakje + eind_prijs_caramel_hoorntje
+        eind_prijs_aardbei = round(aantal_aardbei * prijs_bolletjes,2)
+        eind_prijs_chocola = round(aantal_chocolade * prijs_bolletjes,2)
+        eind_prijs_vanille = round(aantal_vanille * prijs_bolletjes,2)
+        eind_prijs_hoorntjes = round(aantal_hoorntjes * prijs_hoorntjes,2)
+        eind_prijs_bakjes = round(aantal_bakjes * prijs_bakjes,2)
+        eind_prijs_totaal = round(eind_prijs_bakjes + eind_prijs_hoorntjes + eind_prijs_aardbei + eind_prijs_chocola + eind_prijs_vanille + eind_prijs_topping,2)
+    elif klant == 2:
+        aantal_liter = aantal_bolletjes_ijs(klant)
+        for liters in range(aantal_liter):
+            smaken = smaak(aantal_bolletjes, aantal_liter, klant)
+            aantal_aardbei, aantal_chocolade, aantal_vanille = aantal_smaak(aantal_aardbei,aantal_chocolade,aantal_vanille,smaken)
+        eind_prijs_aardbei = round(aantal_aardbei * prijs_liter,2)
+        eind_prijs_chocola = round(aantal_chocolade * prijs_liter,2)
+        eind_prijs_vanille = round(aantal_vanille * prijs_liter,2)
+        eind_prijs_ijs = eind_prijs_aardbei + eind_prijs_chocola + eind_prijs_vanille
+        eind_btw = round(eind_prijs_ijs / 100 * btw,2)
+        print(eind_btw)
+        eind_prijs_totaal = round(eind_prijs_aardbei + eind_prijs_chocola + eind_prijs_vanille + eind_btw,2)
+        volgende_bestelling = 'nee'
+
+if klant == 1:
+    print('-----------["papi gelato"]------------')
+    if aantal_aardbei >= 1:
+        print(f'Bolletjes aardbei      {aantal_aardbei} x ${prijs_bolletjes} = $ {eind_prijs_aardbei}')
+    if aantal_chocolade >= 1:
+        print(f'Bolletjes chocola     {aantal_chocolade} x ${prijs_bolletjes} = $ {eind_prijs_chocola}')
+    if aantal_vanille >= 1:
+        print(f'Bolletjes vanille      {aantal_vanille} x ${prijs_bolletjes} = $ {eind_prijs_vanille}')
+    if aantal_hoorntjes >= 1:
+            print(f'Hoorntjes                 {aantal_hoorntjes} x ${prijs_hoorntjes} = $ {eind_prijs_hoorntjes}')
+    if aantal_bakjes >= 1:
+            print(f'Bakjes         {aantal_bakjes} x ${prijs_bakjes} = $ {eind_prijs_bakjes}')
+    if aantal_topping >= 1:
+            print(f'topping                        = $ {eind_prijs_topping}')
+    print('---------------------------------------')
+    print(f'totaal         = ${eind_prijs_totaal}')
+    print('bedankt en tot ziens')
+elif klant == 2:
+    print('-----------["papi gelato"]------------')
+    if aantal_aardbei >= 1:
+        print(f'liter aardbei      {aantal_aardbei} x ${prijs_liter} = $ {eind_prijs_aardbei}')
+    if aantal_chocolade >= 1:
+        print(f'liter chocola     {aantal_chocolade} x ${prijs_liter} = $ {eind_prijs_chocola}')
+    if aantal_vanille >= 1:
+        print(f'liter vanille      {aantal_vanille} x ${prijs_liter} = $ {eind_prijs_vanille}')
+    print(f'BTW                             = $ {eind_btw}')
+    print('---------------------------------------')
+    print(f'totaal         = ${eind_prijs_totaal}')
+    print('bedankt en tot ziens')
+
+     
+     
--- Module 5 - meer functions/ijssalon/library_ijssalon.py
+++ Module 5 - meer functions/ijssalon/library_ijssalon.py
@@ -1,25 +1,21 @@
-prijs_bolletjes = 1.10
-prijs_hoorntjes = 1.25
-prijs_bakjes = 0.75
-aantal_bolletjes = 0
-aantal_hoorntjes = 0
-aantal_bakjes = 0 
-volgende_bestelling = 'ja'
-hoorn_of_bak = ''
-aantal = 0
-aantal_aardbei = 0
-aantal_chocolade = 0
-aantal_munt = 0 
-aantal_vanille = 0
 volgende_bestelling = 'ja'
 
-def aantal_bolletjes_ijs():
-    aantal_bolletjes = int(input('hoeveel bolletjes'))
-    print(aantal_bolletjes)
-    while aantal_bolletjes > 8:
-        print('sorry zulke grote bakjes hebben we niet')
-        aantal_bolletjes = int(input('hoeveel bolletjes wilt u?'))
-    return aantal_bolletjes
+def soort_klant():
+    soort_klant = input('bent u 1) een particuliere klant of 2) een zakelijke klant?')
+    return soort_klant
+
+def aantal_bolletjes_ijs(klant):
+    if klant == 1:
+        aantal_bolletjes = int(input('hoeveel bolletjes'))
+        print(f'{aantal_bolletjes}aantal bolletjes')
+        while aantal_bolletjes > 8:
+            print('sorry zulke grote bakjes hebben we niet')
+            aantal_bolletjes = int(input('hoeveel bolletjes wilt u?'))
+        return aantal_bolletjes 
+    else:
+        aantal_liter = int(input('hoeveel liter'))
+        print(f'{aantal_liter}aantal liter')
+    return aantal_liter
 
 def waar_in(aantal_bolletjes):
     hofb = ''
@@ -42,19 +38,44 @@ def meer():
         print('bedankt en tot ziens')
     return doorgaan
 
-def smaak(aantal_bolletjes, bolletje):
-    smaak = input(f'welke smaak wilt u voor bolletje {bolletje}? A) aardbei, C) chocolade, M) munt of V) vanille?')
+def smaak(bolletje, liter, klant):
+    if klant == 1:
+        smaak = input(f'welke smaak wilt u voor bolletje {bolletje}? A) aardbei, C) chocolade, of V) vanille?')
+    elif klant == 2:
+        smaak = input(f'welke smaak wilt u voor liter {liter}? A) aardbei, C) chocolade, of V) vanille?')
     smaak=smaak.upper()
-    print(smaak) 
     return smaak
 
-def aantal_smaak(aantal_aardbei, aantal_chocolade, aantal_munt, aantal_vanille,taste):
+def aantal_smaak(aantal_aardbei, aantal_chocolade, aantal_vanille, taste):
     if taste == 'A':
         aantal_aardbei +=1
     elif taste == 'C':
         aantal_chocolade +=1
-    elif taste == 'M':
-        aantal_munt +=1
     elif taste == 'V':
         aantal_vanille +=1
-    return aantal_aardbei, aantal_chocolade, aantal_munt, aantal_vanille
+    return aantal_aardbei, aantal_chocolade, aantal_vanille
+
+def welke_topping(bolletjes,bofh):
+    aantal_sprinkels = 0
+    aantal_slagroom = 0
+    aantal_caramel_bak = 0 
+    aantal_caramel_hoorn = 0
+    aantal_geen =0 
+    topping = input(f'wat voor topping wilt u: A) geen, B) slagroom, C) sprinkels of D) caramel saus?')
+    topping = topping.upper()
+    if topping == 'C':
+        for bolletjes in range(bolletjes):
+            extra_sprinkels = input(f'wilt uw op bolletje {bolletjes+1} sprinkels')
+            if extra_sprinkels == 'ja':
+                aantal_sprinkels +=1 
+    elif topping == 'A':
+        aantal_geen += 1
+    elif topping == 'B':
+        aantal_slagroom +=1 
+    else: 
+        if bofh == 'hoorntje':
+            aantal_caramel_hoorn +=1
+        elif bofh == 'bakje':
+            aantal_caramel_bak += 1 
+    return aantal_geen, aantal_slagroom, aantal_sprinkels, aantal_caramel_bak, aantal_caramel_hoorn
+
